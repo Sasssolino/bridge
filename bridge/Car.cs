cf51a4c8993b734ac0b1412b872befdd6a03b8da
paolo.magnani <paolo.magnani@PC0112>
2023-02-18 12:58:32 +0100

    initial commit

---
+++ bridge/Car.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bridge
+{
+    internal class Car : IWritableItem
+    {
+        private Thread thread;
+        private int _plate;
+        public int PrevPosition { get; set; }
+        public List<string> Texture { get; set; }
+        public int Plate
+        {
+            get { return _plate; }
+            set
+            {
+                if (value < 0)
+                    throw new Exception("car: invalid plate number");
+
+                _plate = value;
+            }
+        }
+
+        private int velocity;
+
+        public bool IsFinished { get; set; }
+
+        private IOutput _out;
+
+        public Car(int num, IOutput output)
+        {
+            Plate = num;
+            _out = output;
+            PrevPosition = 0;
+            Texture = new List<string>();
+        }
+
+        public void Start(int nChannel)
+        {
+            
+        }
+
+        public int GetPosition()
+        {
+            return 0;
+        }
+
+
+    }
+
+    public enum CarState
+    {
+        running, unstarted, 
+    }
+}
---
+++ bridge/OutputSystem.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bridge
+{
+    internal class OutputSystem : IOutput
+    {
+        Thread th;
+        public SemaphoreSlim consoleSem;
+        public SemaphoreSlim bufferSem;
+        int _lenBuffer;
+        public int LenBuffer 
+        { 
+            get { return _lenBuffer; } 
+            set
+            {
+                if(value < 1) 
+                    throw new Exception("OutSys: buffer must have at least one item");
+            
+                _lenBuffer = value;
+            }
+        }
+
+        public List<IWritableItem> buffer { get; set; }
+
+        public OutputSystem(int lenBuffer)
+        {
+            LenBuffer = lenBuffer;
+            consoleSem = new SemaphoreSlim(0, 1);
+            bufferSem = new SemaphoreSlim(0, LenBuffer);
+        }
+
+        public void StartPrintBuffer()
+        {
+            th = new Thread(new ThreadStart(PrintBuffer));
+            th.Start();
+        }
+        public void StopPrintBuffer()
+        {
+#pragma warning disable SYSLIB0006
+            th.Abort();
+#pragma warning restore SYSLIB0006
+        }
+
+        public void AddItemToBuffer(IWritableItem item)
+        {
+            bufferSem.Wait();
+            buffer.Add(item);
+        }
+
+        public void PrintBuffer()
+        {
+            // mettere una variabile per la condizione
+            while (true)
+            {
+                // turn int method
+                for(int i = 0; i < buffer.Count; i++)
+                {
+                    if (!buffer[i].IsFinished)
+                        RemoveItemFromBuffer(buffer[i]);
+
+                    EraseCar(buffer[i]);
+                    PrintCar(buffer[i]);
+                }
+            }
+        }
+
+        public void RemoveItemFromBuffer(IWritableItem item)
+        {
+            buffer.Remove(item);
+            bufferSem.Release();
+        }
+
+        public void EraseCar(IWritableItem item)
+        {
+
+        }
+
+        public void PrintCar(IWritableItem item)
+        {
+
+        }
+
+        public void PrintBridge()
+        {
+
+        }
+
+        public void Write(string message, int x = 0, int y = 0)
+        {
+
+        }
+    }
+
+    internal interface IOutput
+    {
+        public void AddItemToBuffer(IWritableItem obj);
+        public void StartPrintBuffer();
+        public void StopPrintBuffer();
+    }
+
+    internal interface IWritableItem
+    {
+        public bool IsFinished { get; set; }
+        public List<string> Texture { get; set; }
+        public int GetPosition();
+        public int PrevPosition { get; set; }
+    }
+}
